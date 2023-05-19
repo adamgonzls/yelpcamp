63719986c2d40ee7dc464b6d43f60641f446ead3
Adam Gonzales <adamgonzales1@gmail.com>
2023-05-19 16:51:06 -0600

    feature: add update and delete abilities - update templates for update and delete

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 const Campground = require('./models/campground')
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(__dirname + '/public'))
+app.use(methodOverride('_method'))
 
 // view homepage
 app.get('/', async (req, res) => {
@@ -38,6 +40,34 @@ app.get('/campgrounds/:id', async (req, res) => {
   const { id } = req.params
   const campground = await Campground.findById(id)
   res.render('campgrounds/details', { campground })
+})
+
+// edit campground page
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const { id } = req.params
+  const campground = await Campground.findById(id)
+  res.render('campgrounds/edit', { campground })
+})
+
+// process campground edits
+app.put('/campgrounds/:id', async (req, res) => {
+  console.log(req.body)
+  const { id } = req.params
+  const campground = await Campground.findByIdAndUpdate(
+    id,
+    req.body.campground,
+    {
+      runValidators: true,
+      new: true,
+    }
+  )
+  res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params
+  const deleteThis = await Campground.findByIdAndDelete(id)
+  res.redirect('/campgrounds')
 })
 
 mongoose
